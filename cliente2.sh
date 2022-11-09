18660917b83bc67ba5059ed613e0db74739ed3f3
CastillaJesus <jesus.castilla@enti.cat>
2022-11-09 11:12:13 +0100

    Comprobaciones y vacas

--- cliente2.sh
+++
@@ -1,7 +0,0 @@
-#!bin/bash
-
-echo "Cliente TURIP"
-
-echo "HOLI_TURIP" | nc localhost 2223 
-
-
--- servidor.sh
+++ servidor.sh
@@ -1,18 +1,52 @@
 #!/bin/bash
 
+
+PORT="2225"
+
 echo "Servidor Transfer Unite Recursive International Protocol: TURIP"
 
 
+echo "(0) LISTEN Handshake"
 
-echo "(0) LISTEN"
+MSG=`nc -l $PORT`
 
-MSG=`nc -l 2223` 
+HANDSHAKE=`echo $MSG | cut -d " " -f 1`
+IP_CLIENT=`echo $MSG | cut -d " " -f 2`
 
-if [ "$MSG != "HOLI_TURIP ]
+echo"(3) SEND: Comprobación"
+
+if [ "$HANDSHAKE" != "HOLI_TURIP" ]
 then
     echo "ERROR 1: Handshake incorrecto"
-    exit 1
-fi 
+
+    echo "KO_TURIP" | nc $IP_CLIENT $PORT
+
+	exit 1
+fi
+
+echo "OK_TURIP" | nc $IP_CLIENT $PORT
+
+echo "(4) LISTEN"
+
+MSG=`nc -l $PORT`
+
+PREFIX=`echo $MSG | cut-d " " -f 1`
+FILE_NAME=`echo $MSG | cut-d " " -f 2`
+
+echo "(7) SEND: Comprobación nombre archivo"
+
+if [ "$PREFIX" != "FILE_NAME" ]
+then
+
+echo "ERROR 2: Nombre de archivo mal formado"
+
+echo "KO_FILE_NAME" | nc $IP_CLIENT $PORT
+
+exit 2
+
+fi
+
+echo "OK_FILE_NAME" | nc $IP_CLIENT $PORT
 
 
-
+exit 0
